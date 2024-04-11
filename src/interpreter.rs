9778a09308870abb2f43b054060eeed7ef26146f
David Emmel <dgemmel2@gmail.com>
2024-04-10 22:17:24 -0700

    Add quotes to string value display

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -16,7 +16,7 @@ impl Display for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Value::Number(n) => write!(f, "{}", n),
-            Value::String(s) => write!(f, "{}", s),
+            Value::String(s) => write!(f, "\"{}\"", s),
             Value::Boolean(b) => write!(f, "{}", b),
             Value::Nil => write!(f, "nil"),
         }
