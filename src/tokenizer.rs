68f89dfbfc19edcab8b8d63d31242d196e062fa0
David Emmel <dgemmel2@gmail.com>
2024-04-12 00:30:36 -0700

    Allow adding span

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -62,6 +62,30 @@ pub struct Span {
     pub end_column: usize,
 }
 
+impl std::ops::Add<Span> for Span {
+    type Output = Span;
+
+    fn add(self, other: Span) -> Span {
+        let start = if self.start_line < other.start_line {
+            &self
+        } else {
+            &other
+        };
+        let end = if self.end_line > other.end_line {
+            &self
+        } else {
+            &other
+        };
+
+        Span {
+            start_line: start.start_line,
+            start_column: start.start_column,
+            end_line: end.end_line,
+            end_column: end.end_column,
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Token {
     pub token_type: TokenType,
