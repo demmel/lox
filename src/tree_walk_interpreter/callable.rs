897c9ef53531fb65a24521a1d799c2d0d0f3b5a7
David Emmel <dgemmel2@gmail.com>
2024-05-02 19:21:42 -0700

    Improve argument evaluation for small functions by avoiding allocation

--- src/tree_walk_interpreter/callable.rs
+++ src/tree_walk_interpreter/callable.rs
@@ -9,6 +9,85 @@ use super::{
     Class, ExecutionErrorKind, Instance, Interpreter, Value,
 };
 
+enum EvaluatedArgs {
+    Zero,
+    One(Value),
+    Two(Value, Value),
+    Three(Value, Value, Value),
+    Many(Vec<Value>),
+}
+
+impl EvaluatedArgs {
+    fn from_expressions(
+        interpreter: &mut Interpreter,
+        args: &[Expression],
+    ) -> Result<Self, ExecutionErrorKind> {
+        match args.len() {
+            0 => Ok(Self::Zero),
+            1 => Ok(Self::One(interpreter.evaluate(&args[0])?)),
+            2 => Ok(Self::Two(
+                interpreter.evaluate(&args[0])?,
+                interpreter.evaluate(&args[1])?,
+            )),
+            3 => Ok(Self::Three(
+                interpreter.evaluate(&args[0])?,
+                interpreter.evaluate(&args[1])?,
+                interpreter.evaluate(&args[2])?,
+            )),
+            _ => Ok(Self::Many(
+                args.iter()
+                    .map(|arg| interpreter.evaluate(arg))
+                    .collect::<Result<Vec<_>, _>>()?,
+            )),
+        }
+    }
+
+    fn bind_values(
+        &self,
+        scope: &Rc<RefCell<Scope>>,
+        decl: &FunctionDecl,
+    ) -> Result<(), ExecutionErrorKind> {
+        match self {
+            Self::Zero => Ok(()),
+            Self::One(value) => {
+                scope
+                    .borrow_mut()
+                    .declare(decl.args[0].clone(), Declarable::Variable(value.clone()))?;
+                Ok(())
+            }
+            Self::Two(value1, value2) => {
+                scope
+                    .borrow_mut()
+                    .declare(decl.args[0].clone(), Declarable::Variable(value1.clone()))?;
+                scope
+                    .borrow_mut()
+                    .declare(decl.args[1].clone(), Declarable::Variable(value2.clone()))?;
+                Ok(())
+            }
+            Self::Three(value1, value2, value3) => {
+                scope
+                    .borrow_mut()
+                    .declare(decl.args[0].clone(), Declarable::Variable(value1.clone()))?;
+                scope
+                    .borrow_mut()
+                    .declare(decl.args[1].clone(), Declarable::Variable(value2.clone()))?;
+                scope
+                    .borrow_mut()
+                    .declare(decl.args[2].clone(), Declarable::Variable(value3.clone()))?;
+                Ok(())
+            }
+            Self::Many(values) => {
+                for (arg_name, value) in decl.args.iter().zip(values.iter()) {
+                    scope
+                        .borrow_mut()
+                        .declare(arg_name.clone(), Declarable::Variable(value.clone()))?;
+                }
+                Ok(())
+            }
+        }
+    }
+}
+
 #[derive(Clone)]
 pub struct CallableFunction {
     pub scope: Rc<RefCell<Scope>>,
@@ -45,22 +124,13 @@ impl CallableFunction {
         interpreter: &mut Interpreter,
         args: &[Expression],
     ) -> Result<Value, ExecutionErrorKind> {
-        let evaluated_args = args
-            .iter()
-            .map(|arg| interpreter.evaluate(arg))
-            .collect::<Result<Vec<_>, _>>()?;
+        let evaluated_args = EvaluatedArgs::from_expressions(interpreter, args)?;
 
         let res = interpreter.execute_in_scope(
             Scope::boxed(Some(self.scope.clone()), true),
             |interpreter| {
-                for (arg_name, evaluated_arg) in
-                    self.decl.args.iter().zip(evaluated_args.into_iter())
-                {
-                    interpreter
-                        .scope
-                        .borrow_mut()
-                        .declare(arg_name.clone(), Declarable::Variable(evaluated_arg))?;
-                }
+                evaluated_args.bind_values(&interpreter.scope, &self.decl)?;
+
                 let res = interpreter.execute(&self.decl.body)?.unwrap_or(Value::Nil);
                 if self.is_initializer {
                     match Scope::get_at(interpreter.scope.clone(), 1, "this") {
